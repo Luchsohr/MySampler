b9cf269da2a649656a1d83882dbc76de0aeaa301
Luchsohr <72302886+Luchsohr@users.noreply.github.com>
2020-10-30 11:39:55 +0100

    second

--- app/src/main/java/com/example/mysampler/MainActivity.java
+++ app/src/main/java/com/example/mysampler/MainActivity.java
@@ -11,17 +11,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.SoundPool;
 import android.widget.Toast;
-
+import android.widget.ImageView;
 //public class MainActivity extends AppCompatActivity {
 public class MainActivity extends ComponentActivity {
 
-    private Button Button1;
-    private Button Button2;
-    private Button Button3;
-    private Button Button1b;
-    private Button Button2b;
-    private Button Button3b;
+
     private SoundPool soundPool;
+    private ImageView SheepView1;
+    private ImageView SheepView2;
+    private ImageView SheepView3;
+    private ImageView SheepView1b;
+    private ImageView SheepView2b;
+    private ImageView SheepView3b;
     int  SheepSoundID1 ;
     int  SheepSoundID2 ;
     int  SheepSoundID3 ;
@@ -32,41 +33,12 @@ public class MainActivity extends ComponentActivity {
         einToast.show();
     }
 
-    public void PlayMySound(int id)
-    {
-        MediaPlayer myPlayer = MediaPlayer.create(this,id);
-        myPlayer.start();
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button1 = findViewById(R.id.BUTTON_ID_1);
-        Button1.setOnClickListener(new View.OnClickListener(){
-           public void onClick(View v)
-           {
-               PlayMySound(R.raw.superschaaf1);
-           }
-        });
-
-        Button2 = findViewById(R.id.BUTTON_ID_2);
-        Button2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v)
-            {
-                PlayMySound(R.raw.superschaaf2);
-            }
-        });
-
-        Button3 = findViewById(R.id.BUTTON_ID_3);
-        Button3.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v)
-            {
-                PlayMySound(R.raw.superschaaf3);
-            }
-        });
 
         soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
         soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
@@ -81,27 +53,47 @@ public class MainActivity extends ComponentActivity {
         SheepSoundID3 = soundPool.load(this,R.raw.superschaaf3, 1);
 
 
-        Button1b = findViewById(R.id.BUTTON_ID_1B);
-        Button1b.setOnClickListener(new View.OnClickListener(){
+        SheepView1 = findViewById(R.id.SHEEP_ID_1);
+        SheepView1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v)
             {
                 soundPool.play(SheepSoundID1, 1.0f, 1.0f, 1, 0, 1.0f);
             }
         });
-
-        Button2b = findViewById(R.id.BUTTON_ID_2B);
-        Button2b.setOnClickListener(new View.OnClickListener(){
+        SheepView2 = findViewById(R.id.SHEEP_ID_2);
+        SheepView2.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v)
             {
                 soundPool.play(SheepSoundID2, 1.0f, 1.0f, 1, 0, 1.0f);
             }
         });
-
-        Button3b = findViewById(R.id.BUTTON_ID_3B);
-        Button3b.setOnClickListener(new View.OnClickListener(){
+        SheepView3 = findViewById(R.id.SHEEP_ID_3);
+        SheepView3.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v)
             {
                 soundPool.play(SheepSoundID3, 1.0f, 1.0f, 1, 0, 1.0f);
+            }
+        });
+
+        SheepView1b = findViewById(R.id.SHEEP_ID_1b);
+        SheepView1b.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                soundPool.play(SheepSoundID1, 1.0f, 1.0f, 1, 0, 2.0f);
+            }
+        });
+        SheepView2b = findViewById(R.id.SHEEP_ID_2b);
+        SheepView2b.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                soundPool.play(SheepSoundID2, 1.0f, 1.0f, 1, 0, 2.0f);
+            }
+        });
+        SheepView3b = findViewById(R.id.SHEEP_ID_3b);
+        SheepView3b.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                soundPool.play(SheepSoundID3, 1.0f, 1.0f, 1, 0, 2.0f);
             }
         });
     };
